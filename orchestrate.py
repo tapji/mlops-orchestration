139000f32796e2b7cb0f004c3b611a144aa08fe6
Tapji Fwangwen <126870311+tapji@users.noreply.github.com>
2023-06-10 13:27:02 +0100

    Update orchestrate.py

--- orchestrate.py
+++ orchestrate.py
@@ -111,8 +111,8 @@ def train_best_model(
 
 @flow
 def main_flow(
-    train_path: str = "tapji/mlops-orchestration/blob/main/green_tripdata_2023-01.parquet",
-    val_path: str = "tapji/mlops-orchestration/blob/main/green_tripdata_2023-01.parquet",
+    train_path: str = "./data/green_tripdata_2023-01.parquet",
+    val_path: str = "./data/green_tripdata_2023-01.parquet",
 ) -> None:
     """The main training pipeline"""
 
